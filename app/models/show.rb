1ed0654ab731c4e19255db23b6b819431fcefddb
Shun Yao <ysmiracle@hotmail.com>
2018-04-17 01:24:46 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -6,4 +6,17 @@ class Show < ActiveRecord::Base
   def self.most_popular_show
     Show.where(rating: self.highest_rating).first
   end
+
+  def self.lowest_rating
+    Show.minimum(:rating)
+  end
+
+  def self.least_popular_show
+    Show.where(rating: self.lowest_rating).first
+  end
+
+  def self.ratings_sum
+    Show.sum(:rating)
+  end
+
 end
