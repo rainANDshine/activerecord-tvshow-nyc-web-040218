b5c43361cb794a8f7c5749bb82c530e3c2efd86a
Shun Yao <ysmiracle@hotmail.com>
2018-04-17 01:09:36 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -4,6 +4,7 @@ class Show < ActiveRecord::Base
   end
 
   def self.most_popular_show
-    Show.where(rating: self.highest_rating)
+    rating = Show.where(rating: self.highest_rating)
+    rating.flatten
   end
 end
